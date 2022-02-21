d6f8768ee3f724d1fb0e8c0d9bd51abc26cbd7eb
Cytty <cytty42@gmail.com>
2022-02-21 20:12:31 +0300

    Added old method

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -2,7 +2,66 @@ public class Game {
     public static boolean trafficGreenLight = false;
     public static int maxSpeed = 5;
 
-    public static String[] listPlayers(String[] players) {
+    public static void printDelim(String var) {  // выводится вариант фильтра
+        System.out.println("---------------------------------");
+        System.out.println(var);
+    }
+
+    public static int moves(int[] speeds) {   // расчет количества выбывающих с учетом скорости движения
+        int quantityLosers = 0;
+        if (trafficGreenLight == false) {
+            for (int speed : speeds) {
+                if (Math.abs(speed) > Math.abs(maxSpeed)) {
+                    quantityLosers++;
+                }
+            }
+        }
+        return quantityLosers;
+    }
+
+    public static int[] losers(int[] speeds) { // формирование списка скоростей выбывающих с учетом скорости движения
+        int quantityLosers = moves(speeds);
+        int[] quantitySpeedLosers = new int[quantityLosers];
+        int i = 0;
+        if (quantityLosers != 0) {
+            for (int speed : speeds) {
+                if (Math.abs(speed) > Math.abs(maxSpeed)) {
+                    quantitySpeedLosers[i] = speed;
+                    i++;
+                }
+            }
+        }
+        return quantitySpeedLosers;
+    }
+
+    public static int[] winners(int[] speeds) { // формирование списка скоростей победителей
+        int[] quantitySpeedWinners;
+        if (trafficGreenLight == true) {
+            quantitySpeedWinners = speeds;
+        } else {
+            int quantityWinners = 0;
+            int i = 0;
+            for (int speed : speeds) {
+                if (Math.abs(speed) <= Math.abs(maxSpeed)) {
+                    quantityWinners++;
+                    i++;
+                }
+            }
+            quantitySpeedWinners = new int[quantityWinners];
+            i = 0;
+            if (quantityWinners != 0) {
+                for (int speed : speeds) {
+                    if (Math.abs(speed) <= Math.abs(maxSpeed)) {
+                        quantitySpeedWinners[i] = speed;
+                        i++;
+                    }
+                }
+            }
+        }
+        return quantitySpeedWinners;
+    }
+
+    public static String[] listPlayers(String[] players) { // формирование списка имен победителей
         String[] nameWin;
         int quantityWin = 0;
         for (String player : players) {
@@ -26,4 +85,3 @@ public class Game {
         return nameWin;
     }
 }
-
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -4,6 +4,247 @@ import org.junit.jupiter.api.Test;
 public class GameTest {
 
     @Test
+    public void movesShouldJobBigNumberRed() {
+        int[] speed = {7, 5, 6, 2_147_483_647};
+        Game.trafficGreenLight = false;
+        Game.maxSpeed = 6;
+        int expected = 2;
+        int actual = Game.moves(speed);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void movesShouldJobBigNumberGreen() {
+        int[] speed = {7, 5, 6, 2_147_483_647};
+        Game.trafficGreenLight = true;
+        Game.maxSpeed = 6;
+        int expected = 0;
+        int actual = Game.moves(speed);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void movesShouldCountNegativeRed() {
+        int[] speed = {-7, 5, 6, 10};
+        Game.trafficGreenLight = false;
+        Game.maxSpeed = 6;
+        int expected = 2;
+        int actual = Game.moves(speed);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void movesShouldCountNegativeGreen() {
+        int[] speed = {-7, 5, 6, 10};
+        Game.trafficGreenLight = true;
+        Game.maxSpeed = 6;
+        int expected = 0;
+        int actual = Game.moves(speed);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void movesShouldJobNegativeMaxSpeedRed() {
+        int[] speed = {7, 5, 6, 10};
+        Game.trafficGreenLight = false;
+        Game.maxSpeed = -6;
+        int expected = 2;
+        int actual = Game.moves(speed);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void movesShouldJobNegativeMaxSpeedGreen() {
+        int[] speed = {7, 5, 6, 10};
+        Game.trafficGreenLight = true;
+        Game.maxSpeed = -6;
+        int expected = 0;
+        int actual = Game.moves(speed);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void movesNegativeMaxSpeedAndNegativeSpeedRed() {
+        int[] speed = {7, -5, 6, -7, 0, 5, -6};
+        Game.trafficGreenLight = false;
+        Game.maxSpeed = -6;
+        int expected = 2;
+        int actual = Game.moves(speed);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void movesNegativeMaxSpeedAndNegativeSpeedGreen() {
+        int[] speed = {7, -5, 6, -7, 0, 5, -6};
+        Game.trafficGreenLight = true;
+        Game.maxSpeed = -6;
+        int expected = 0;
+        int actual = Game.moves(speed);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void losersShouldJobBigNumberRed() {
+        int[] speed = {7, 5, 6, 2_147_483_647};
+        Game.trafficGreenLight = false;
+        Game.maxSpeed = 6;
+        int[] expected = {7, 2_147_483_647};
+        int[] actual = Game.losers(speed);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void losersShouldJobBigNumberGreen() {
+        int[] speed = {7, 5, 6, 2_147_483_647};
+        Game.trafficGreenLight = true;
+        Game.maxSpeed = 6;
+        int[] expected = {};
+        int[] actual = Game.losers(speed);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void losersShouldCountNegativeRed() {
+        int[] speed = {0, 5, 10, 4, -3, -7};
+        Game.trafficGreenLight = false;
+        Game.maxSpeed = 6;
+        int[] expected = {10, -7};
+        int[] actual = Game.losers(speed);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void losersShouldCountNegativeGreen() {
+        int[] speed = {0, 5, 10, 4, -3, -7};
+        Game.trafficGreenLight = true;
+        Game.maxSpeed = 6;
+        int[] expected = {};
+        int[] actual = Game.losers(speed);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void losersShouldJobNegativeMaxSpeedRed() {
+        int[] speed = {0, 5, 10, 4, 13, 7};
+        Game.trafficGreenLight = false;
+        Game.maxSpeed = -6;
+        int[] expected = {10, 13, 7};
+        int[] actual = Game.losers(speed);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void losersShouldJobNegativeMaxSpeedGreen() {
+        int[] speed = {0, 5, 10, 4, 13, 7};
+        Game.trafficGreenLight = true;
+        Game.maxSpeed = -6;
+        int[] expected = {};
+        int[] actual = Game.losers(speed);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void losersNegativeMaxSpeedAndNegativeSpeedRed() {
+        int[] speed = {7, -5, 6, -7, 0, 5, -6};
+        Game.trafficGreenLight = false;
+        Game.maxSpeed = -6;
+        int[] expected = {7, -7};
+        int[] actual = Game.losers(speed);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void losersNegativeMaxSpeedAndNegativeSpeedGreen() {
+        int[] speed = {7, -5, 6, -7, 0, 5, -6};
+        Game.trafficGreenLight = true;
+        Game.maxSpeed = -6;
+        int[] expected = {};
+        int[] actual = Game.losers(speed);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void winnersShouldJobBigNumberRed() {
+        int[] speed = {7, 5, 6, 2_147_483_647};
+        Game.trafficGreenLight = false;
+        Game.maxSpeed = 6;
+        int[] expected = {5, 6};
+        int[] actual = Game.winners(speed);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void winnersShouldJobBigNumberGreen() {
+        int[] speed = {7, 5, 6, 2_147_483_647};
+        Game.trafficGreenLight = true;
+        Game.maxSpeed = 6;
+        int[] expected = {7, 5, 6, 2_147_483_647};
+        int[] actual = Game.winners(speed);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void winnersShouldCountNegativeRed() {
+        int[] speed = {0, 5, 10, 4, -3, -7};
+        Game.trafficGreenLight = false;
+        Game.maxSpeed = 6;
+        int[] expected = {0, 5, 4, -3};
+        int[] actual = Game.winners(speed);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void winnersShouldCountNegativeGreen() {
+        int[] speed = {0, 5, 10, 4, -3, -7};
+        Game.trafficGreenLight = true;
+        Game.maxSpeed = 6;
+        int[] expected = {0, 5, 10, 4, -3, -7};
+        int[] actual = Game.winners(speed);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void winnersShouldJobNegativeMaxSpeedRed() {
+        int[] speed = {0, 5, 10, 4, 3, 7};
+        Game.trafficGreenLight = false;
+        Game.maxSpeed = -6;
+        int[] expected = {0, 5, 4, 3};
+        int[] actual = Game.winners(speed);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void winnersShouldJobNegativeMaxSpeedGreen() {
+        int[] speed = {0, 5, 10, 4, 3, 7};
+        Game.trafficGreenLight = true;
+        Game.maxSpeed = -6;
+        int[] expected = {0, 5, 10, 4, 3, 7};
+        int[] actual = Game.winners(speed);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void winnersNegativeMaxSpeedAndNegativeSpeedRed() {
+        int[] speed = {0, 5, 10, 4, -3, -7};
+        Game.trafficGreenLight = false;
+        Game.maxSpeed = -6;
+        int[] expected = {0, 5, 4, -3};
+        int[] actual = Game.winners(speed);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void winnersNegativeMaxSpeedAndNegativeSpeedGreen() {
+        int[] speed = {0, 5, 10, 4, -3, -7};
+        Game.trafficGreenLight = true;
+        Game.maxSpeed = -6;
+        int[] expected = {0, 5, 10, 4, -3, -7};
+        int[] actual = Game.winners(speed);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+
+    @Test
     public void listNameWinNegativeAllSpeedGreen() {
         Game.trafficGreenLight = true;
         Game.maxSpeed = -3;
@@ -217,6 +458,4 @@ public class GameTest {
         Assertions.assertArrayEquals(expected, actual);
     }
 }
-
-
 
